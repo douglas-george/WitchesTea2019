f755171fe878bbb2872a0cec085f8a4bec6d1094
douglasGeorge <44926301+douglasGeorge@users.noreply.github.com>
2019-09-16 22:40:10 -0400

    Started GUI.

--- TeaServer/WitchesTea2019_TheDarkPoison.py
+++ TeaServer/WitchesTea2019_TheDarkPoison.py
@@ -1,24 +1,45 @@
+import tkinter
 from TeaServer.SmokerServer import SmokerServer
 
+attendees = ["Dennis", "Delta",
+             "Bill", "Kali", "Jaron", "Ammon", "Devor",
+             "Marika", "Alexis", "Amber", "Isaac",
+             "Jessica", "Doug", "Gabriel",
+             "Joe", "Barbara", "Carl",
+             "Alice", "Jeff", "Braeden", "Carter", "Tanner", "Coleman", "Brynlee",
+             "Sam", "Erin", "Sariah", "Emma", "Hyrum",
+             "Courtney", "Hannah", "Luke"]
 
-class TheDarkPoison():
+
+class TheDarkPoison:
     def __init__(self):
-        pass
+        self.root = tkinter.Tk()
 
-    def start(self):
-        print('Starting "The Dark Poison"')
+        self.wandGuiLines = []
+        for attendee in attendees:
+            newWand = Wand(attendee=attendee, root=self.root, timeSinceLastHeartbeat=1000.0)
 
-        print("Creating Servers...")
-        print("\tCreating Smoker Server")
-        self.SmokerServer = SmokerServer()
+        self.root.mainloop()
+
+
+class Wand:
+    def __init__(self, attendee, root, time_since_last_heartbeat):
+        self.gui_line = tkinter.Text(root, height=1, width=10)
+        self.gui_line.insert(tkinter.INSERT, attendee)
+        self.gui_line.pack()
+
+    def get_row_color(self, time_since_last_heartbeat):
+        if time_since_last_heartbeat > 10:
+
+
+
+
 
 
 
 
 if __name__ == "__main__":
     game = TheDarkPoison()
-
-    game.start()
 
 
 
