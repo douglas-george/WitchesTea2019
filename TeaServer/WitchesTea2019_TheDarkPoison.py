762a5c33e64d5e37e029d17bccb0bd4df03f3346
douglas-george <44926301+douglas-george@users.noreply.github.com>
2019-09-17 12:49:37 -0400

    Updated GUI to have a colored row for each wand.

--- TeaServer/WitchesTea2019_TheDarkPoison.py
+++ TeaServer/WitchesTea2019_TheDarkPoison.py
@@ -15,25 +15,56 @@ class TheDarkPoison:
     def __init__(self):
         self.root = tkinter.Tk()
 
+        self.wandFrame = tkinter.Frame(self.root)
+        self.wandFrame.grid(row=0, column=0)
+
+        self.wandLabel = tkinter.Label(self.wandFrame, text="Wand Status", borderwidth=2, relief="groove", width=48)
+        self.wandLabel.grid(row=0, column=0, columnspan=3)
+
         self.wandGuiLines = []
-        for attendee in attendees:
-            newWand = Wand(attendee=attendee, root=self.root, timeSinceLastHeartbeat=1000.0)
+        for index, attendee in enumerate(attendees):
+            newWand = Wand(attendee=attendee, index=(index + 1), root=self.wandFrame, time_since_last_heartbeat=100.0)
 
         self.root.mainloop()
 
 
 class Wand:
-    def __init__(self, attendee, root, time_since_last_heartbeat):
-        self.gui_line = tkinter.Text(root, height=1, width=10)
-        self.gui_line.insert(tkinter.INSERT, attendee)
-        self.gui_line.pack()
+    def __init__(self, attendee, index, root, time_since_last_heartbeat):
+        self.attendee = attendee
+        self.index = index
+        self.root = root
+        self.time_since_last_heartbeat = time_since_last_heartbeat
 
-    def get_row_color(self, time_since_last_heartbeat):
-        if time_since_last_heartbeat > 10:
+        self.wand_name_text = tkinter.Label(root, text=attendee, borderwidth=2, relief="groove", width=15, anchor="nw")
+        self.wand_name_text.grid(row=self.index, column=0)
 
+        self.wand_status_text = tkinter.Label(root, text="uninitialized", borderwidth=2, relief="groove", width=15, anchor="nw")
+        self.wand_status_text.grid(row=self.index, column=1)
 
+        self.wand_time_text = tkinter.Label(root, text=round(time_since_last_heartbeat, 0), borderwidth=2, relief="groove", width=15, anchor="nw")
+        self.wand_time_text.grid(row=self.index, column=2)
 
+        self.update_row_color()
 
+    def update_heartbeat(self, time_since_last_heartbeat):
+        self.time_since_last_heartbeat = time_since_last_heartbeat
+        self.wand_time_text.config({"text": round(time_since_last_heartbeat, 0)})
+        self.update_row_color()
+
+    def update_row_color(self):
+        if self.time_since_last_heartbeat < 2:
+            newColor = "green"
+        elif self.time_since_last_heartbeat < 10:
+            newColor = "yellow"
+        else:
+            newColor = "red"
+
+        self.wand_name_text.config({"background": newColor})
+        self.wand_status_text.config({"background": newColor})
+        self.wand_time_text.config({"background": newColor})
+
+    def update_row_status(self, new_status):
+        self.wand_status_text.config({"text": new_status})
 
 
 
